9baf10e5a5cf0c3807b84af9fdcd808ae6e9adeb
Muhammad Andrean Saputra <andreannproject@gmail.com>
2023-05-31 10:42:09 +0700

    configure view

--- app/init.php
+++ app/init.php
@@ -1,5 +1,5 @@
-<?php 
+<?php
 
 
 require_once 'core/App.php';
-require_once 'core/Controller.php';
+require_once 'core/Controllers.php';
---
+++ app/views/about/index.php
@@ -0,0 +1,4 @@
+<h1>About Me</h1>
+<p>Halo, nama saya <?= $data['nama']; ?>,
+    umur saya <?= $data['pekerjaan']; ?>
+    dan berumur <?= $data['umur']; ?> tahun</p>
