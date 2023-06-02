7f3284bbdcc03fa5e684c7277eda9c8a9d336dea
Muhammad Andrean Saputra <andreannproject@gmail.com>
2023-06-02 22:25:22 +0700

    Insert Data

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -4,6 +4,11 @@
         <div class="col-6">
             <h3>Daftar Mahasiswa</h3>
 
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
+                Tambah data
+            </button>
+
             <ul class="list-group">
                 <?php foreach ($data['mhs'] as $mhs) : ?>
                     <li class="list-group-item d-flex justify-content-between align-items-start">
@@ -15,4 +20,41 @@
         </div>
     </div>
 
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="judulModal">Tambah data mahasiswa</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+                    <div class="mb-3">
+                        <label for="nama" class="form-label">Nama</label>
+                        <input type="text" class="form-control" id="nama" name="nama">
+                    </div>
+                    <div class="mb-3">
+                        <label for="nim" class="form-label">NIM</label>
+                        <input type="number" class="form-control" id="nim" name="nim">
+                    </div>
+                    <div class="mb-3">
+                        <label for="jurusan" class="form-label">Jurusan</label>
+                        <select class="form-select" aria-label="Default select example" id="jurusan" name="jurusan">
+                            <option selected>Pilih Jurusan</option>
+                            <option value="Teknik Otomasi">Teknik Otomasi</option>
+                            <option value="Teknik Mekatronika">Teknik Mekatronika</option>
+                            <option value="Teknik Robotika">Teknik Robotika</option>
+                        </select>
+                    </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Tambah data</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
