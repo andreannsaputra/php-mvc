7ff235866d5fea0410662c728f09b82e8bd18fa1
Muhammad Andrean Saputra <andreannproject@gmail.com>
2023-06-03 09:14:58 +0700

    flasher

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,7 +1,13 @@
 <div class="container mt-4">
 
     <div class="row">
-        <div class="col-6">
+        <div class="col-lg-6">
+            <?php Flasher::flash() ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-6">
             <h3>Daftar Mahasiswa</h3>
 
             <!-- Button trigger modal -->
